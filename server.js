e9f466fcafc596fd533c74a2eb87030e482454c1
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-06-16 18:37:00 -0400

    testing heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -12,6 +13,10 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
