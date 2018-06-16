6660ba93056ddfa3967d139f3c32ce128c0e8807
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-06-16 18:28:22 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
